bfe06160e1c0bada91690f236f20f2c1aa518216
nitika2205 <knitika567@gmail.com>
2026-07-10 12:36:21 +0530

    Register handlers with middleware

--- main.go
+++ main.go
@@ -35,8 +35,11 @@ func main() {
 	cmds.register("reset", handlerReset)
 	cmds.register("users", handlerListUsers)
 	cmds.register("agg", handlerAgg)
-	cmds.register("addfeed", handlerAddFeed)
+	//cmds.register("addfeed", handlerAddFeed)
 	cmds.register("feeds", handlerFeeds)
+	cmds.register("follow", middlewareLoggedIn(handlerFollow))
+	cmds.register("following", middlewareLoggedIn(handlerFollowing))
+	cmds.register("addfeed", middlewareLoggedIn(handlerAddFeed))
 	if len(os.Args) < 2 {
 		log.Fatal("not enough arguments provided")
 	}
